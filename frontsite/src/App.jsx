2f3376405ebb2d6d1df7cc4c1462cf07e21dc05e
ahmadrzq <ahmadrizqiisnain@gmail.com>
2024-01-20 09:57:56 +0700

    Add new routes to App.jsx

--- frontsite/src/App.jsx
+++ frontsite/src/App.jsx
@@ -4,14 +4,25 @@ import {
     Route,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Shop from "./pages/shop/Shop";
+import DetailProduct from "./pages/detailProduct/DetailProduct";
+import Cart from "./pages/cart/Cart";
+import Checkout from "./pages/checkout/Checkout";
+import Contact from "./pages/contact/Contact";
+import Blog from "./pages/blog/Blog";
 
 
 function App() {
-
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/shop" element={<Shop />} />
+                <Route path="/product/:id" element={<DetailProduct />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/blog" element={<Blog />} />
             </Routes>
         </BrowserRouter>
     )

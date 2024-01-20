ed5617f787bad2e316ce3ea6586da7c0d1a7607f
ahmadrzq <ahmadrizqiisnain@gmail.com>
2024-01-20 18:28:29 +0700

    Add navigation bar component

--- frontsite/src/components/front/Navbar.jsx
+++ frontsite/src/components/front/Navbar.jsx
@@ -1,7 +1,68 @@
+import {
+    Link
+} from "react-router-dom";
+import logo from '../../assets/img/logo.png';
+import navUser from '../../assets/img/nav-user.svg';
+import navSearch from '../../assets/img/nav-search.svg';
+import navFavourite from '../../assets/img/nav-favorite.svg';
+import navCart from '../../assets/img/nav-cart.svg';
+import { useState } from "react";
+
 export default function Navbar() {
-  return (
-    <div className="">
-        Navbar
-    </div>
-  )
+    const [menuOpen, setmenuOpen] = useState(false)
+    const [searchOnBar, setsearchOnBar] = useState(false)
+    const handleMenuOpen = () => {
+        setmenuOpen(!menuOpen)
+    }
+    const handleSearchOnBar = () => {
+        setsearchOnBar(!searchOnBar)
+    }
+    return (
+        <div className="p-4 flex justify-between items-center">
+            <div className="logo">
+                <img src={logo} alt="" className='w-36' />
+            </div>
+            <div className="link hidden gap-4">
+                <a href="">Home</a>
+                <a href="">Shop</a>
+                <a href="">About</a>
+                <a href="">Contact</a>
+            </div>
+            <div className="icon hidden gap-4">
+                <img src={navUser} alt="" />
+                <img src={navSearch} alt="" />
+                <img src={navFavourite} alt="" />
+                <img src={navCart} alt="" />
+            </div>
+            <div className={` ${menuOpen ? "right-[-500px]" : "right-0"} listMenu absolute top-0  bg-[#B88E2F] text-white py-10 w-3/4 flex flex-col justify-center transition-all`}>
+                <div className="buttonClose flex justify-center mb-5">
+                    <div onClick={() => {
+                        handleMenuOpen()
+                        setsearchOnBar(false)
+                    }}>
+                        <svg className="cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="48" d="M368 368L144 144M368 144L144 368" /></svg>
+                    </div>
+                </div>
+                <div className="icon flex justify-center gap-4 mb-5">
+                    <img className="cursor-pointer" src={navUser} alt="" />
+                    <img className="cursor-pointer" src={navSearch} alt="" onClick={handleSearchOnBar} />
+                    <img className="cursor-pointer" src={navFavourite} alt="" />
+                    <img className="cursor-pointer" src={navCart} alt="" />
+                </div>
+                <form className={`${searchOnBar ? "" : "hidden"} mb-5 flex flex-col items-center`}>
+                    <input className="w-3/4 p-2 focus:outline-none text-black mb-2 " type="text" />
+                    <button className="bg-white text-[#B88E2F] px-2 py-1 font-semibold" type="submit">Search</button>
+                </form>
+                <div className="link flex flex-col items-center gap-8 text-2xl font-semibold">
+                    <Link className="cursor-pointer" to="/">Home</Link>
+                    <Link className="cursor-pointer" to="/shop">Shop</Link>
+                    <Link className="cursor-pointer" to="/About">About</Link>
+                    <Link className="cursor-pointer" to="/Contact">Contact</Link>
+                </div>
+            </div>
+            <div className="menu" onClick={handleMenuOpen}>
+                <svg className="cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="48" d="M88 152h336M88 256h336M88 360h336" /></svg>
+            </div>
+        </div>
+    )
 }
